e13efc169959369c3c8f20be6e2969ca450e6750
elimerrell <elijah.merrell@gmail.com>
2019-02-19 16:58:45 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,39 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
 
-end
+  get '/reversename/:name' do
+    @name = params[:name]
+    @reversed_name = @name.reverse
+    erb :reversed_name
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    @squared_number = @number * @number
+    erb :squared_number
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    erb :say_number
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    erb :say_words
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    erb :operation
+  end
+
+end #end of App class
